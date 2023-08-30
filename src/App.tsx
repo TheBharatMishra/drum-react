d38e5c51969a8b9e9a9f42a4911633600b3bd26f
TheBharatMishra <mishrabm2004@gmail.com>
2023-08-30 20:44:52 +0530

    Added TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
+    <main className="flex flex-row justify-center items-center h-screen border-8 border-purple-950 bg-red-500">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+          Q
+        </div>
+        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+          A
+        </div>
+        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+          Z
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+          W
+        </div>
+        <div
+          // onKeyDown={(event: Event) => {
+          //   if (event.keyCode === "Q") alert(1);
+          // }}
+          className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded"
+        >
+          <audio src="../assets/Heater-1.mp3"></audio>S
+        </div>
+        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+          X
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div>
+        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+          E
+        </div>
+        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+          D
+        </div>
+        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+          C
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
