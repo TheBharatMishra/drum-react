78c1a6465dcf5a1444b4ac95bf31b652c500f6a0
TheBharatMishra <mishrabm2004@gmail.com>
2023-12-21 21:01:04 +0530

    Assets Moved

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,69 @@
-// import { useState } from "react";
-
 function App() {
-  // const [count, setCount] = useState(0);
+  const playin = () => {
+    // if (e.key == "41")
+    // let toBePlayed: any = document.getElementById("Qel");
+    // console.log(document.getElementsByTagName("div")[4].toString());
+    document.getElementById("Qel")?.play();
+    const dectohex = (decimalnum) => decimalnum.toString(36);
+    // const hextodec = (hexnum) => parseInt(hexnum, 16);
 
+    console.log(dectohex(65));
+    // console.log(document.getElementById("Qel")?.play());s
+
+    // toBePlayed?.play();
+  };
   return (
-    <main className="flex flex-row justify-center items-center h-screen border-8 border-purple-950 bg-red-500">
-      <div>
-        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
-          Q
+    <>
+      <main className="flex flex-row justify-center items-center h-screen border-8 border-purple-950 bg-red-500">
+        <div>
+          <div
+            onClick={playin}
+            className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded cursor-grab"
+          >
+            Q
+          </div>
+          <div
+            id="A"
+            className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded"
+          >
+            A
+          </div>
+          <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+            Z
+          </div>
         </div>
-        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
-          A
+        <div>
+          <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+            W
+          </div>
+          <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+            S
+          </div>
+          <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+            X
+          </div>
         </div>
-        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
-          Z
+        <div>
+          <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+            E
+          </div>
+          <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+            D
+          </div>
+          <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
+            C
+          </div>
+
+          <audio id="Qel" src="src\assets\Heater-1.mp3" />
+          <audio id="Wel" src="assets\Heater-2.mp3" />
+          <audio id="Eel" src="assets\Heater-3.mp3" />
+          <audio id="Ael" src="assets\Heater-4_1.mp3" />
+          <audio id="Ael" src="assets\Heater-6.mp3" />
+          <audio id="" src="assets\Cev_H2.mp3" />
+          <audio src="assets\Cev_H2.mp3" />
         </div>
-      </div>
-      <div>
-        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
-          W
-        </div>
-        <div
-          // onKeyDown={(event: Event) => {
-          //   if (event.keyCode === "Q") alert(1);
-          // }}
-          className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded"
-        >
-          <audio src="../assets/Heater-1.mp3"></audio>S
-        </div>
-        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
-          X
-        </div>
-      </div>
-      <div>
-        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
-          E
-        </div>
-        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
-          D
-        </div>
-        <div className="border-8 p-8 m-4 bg-pink-400 border-purple-900 rounded">
-          C
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
